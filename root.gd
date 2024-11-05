4d16d61a03d5396d182397d159afc9c1fdc6c872
Degus Jr <degus@mail.com>
2024-11-05 19:58:22 +0100

    Add saving, keybinds, performance mode

--- root.gd
+++ root.gd
@@ -2,8 +2,9 @@ extends Control
 
 @onready var file_menu: PopupMenu = $GUI/MenuBar/File
 @onready var settings_menu: PopupMenu = $GUI/MenuBar/Settings
-@onready var file_dialog: FileDialog = $FileDialog
-@onready var save_dialog: FileDialog = $FileDialog2
+@onready var load_dialog: FileDialog = $LoadDialog
+@onready var save_dialog: FileDialog = $SaveDialog
+@onready var warning: AcceptDialog = $Warning
 
 @onready var dm_camera: Camera2D = $Camera
 @onready var dm_fog = $DmRoot/Fog
@@ -18,15 +19,17 @@ extends Control
 
 const LightTexture = preload('res://Light.png')
 const DarkTexture = preload('res://Dark.png')
+const FogTexture = preload('res://fog.jpg')
+
+var current_file_path : String
 
 
 var hovering_over_gui : bool = false
 var mod_held : bool = false
 var black_circle_bool : bool = false
+var performance_mode : bool = false
 
 var brush_size : int = 50
-
-var fog_scaling : float = 1.3
 
 var map_image : Image
 
@@ -42,7 +45,17 @@ var dark_brush : Image
 var m1_pressed: bool = false
 var m2_pressed: bool = false
 
-var prev_mouse_pos = null
+var prev_mouse_pos
+
+const FOG_COLOR_LIST : Array = [
+	null,
+	Color.BLACK,
+	Color.CORNFLOWER_BLUE,
+	Color.DARK_GRAY,
+	Color.GHOST_WHITE,
+]
+
+var fog_color_index : int = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -50,7 +63,7 @@ func _ready() -> void:
 	get_window().title = "DM Window"
 	file_menu.connect('id_pressed', _on_file_id_pressed)
 	settings_menu.connect('id_pressed', _on_settings_id_pressed)
-	file_dialog.add_filter("*.png, *.jpg, *.jpeg, *.map", "Images / .map files")
+	load_dialog.add_filter("*.png, *.jpg, *.jpeg, *.map", "Images / .map files")
 	save_dialog.add_filter("*.map", ".map files")
 	update_brushes()
 
@@ -60,7 +73,7 @@ func _ready() -> void:
 		load_map(args[0])
 
 	else:
-		file_dialog.popup()
+		load_dialog.popup()
 
 
 func update_brushes(value: int = 0) -> void:
@@ -74,12 +87,32 @@ func update_brushes(value: int = 0) -> void:
 		imglist[i].resize(brush_size, brush_size)
 		imglist[i].convert(Image.FORMAT_RGBAH)
 
+func update_mask_wrapper(pos, erase: bool = false):
+	if prev_mouse_pos == null:
+		update_mask(pos, erase)
+		return
+
+	var distance = pos.distance_to(prev_mouse_pos)
+	var step_size = brush_size / 2
+
+	if distance > step_size:
+		var sub_steps = floor(distance / (step_size / 2))
+		var x_diff = pos.x - prev_mouse_pos.x
+		var y_diff = pos.y - prev_mouse_pos.y
 
 
-func update_mask(erase: bool = false):
+		for i in range(1, sub_steps + 1):
+			var sub_pos = pos
+			sub_pos.x -= (x_diff / sub_steps) * i
+			sub_pos.y -= (y_diff / sub_steps) * i
+			update_mask(sub_pos, erase)
+
+	update_mask(pos, erase)
+
+
+
+func update_mask(pos, erase: bool = false):
 	var offset = Vector2.ONE * brush_size / 2
-	var pos = get_global_mouse_position()
-	offset -= Vector2(map_image_width * 0.5, map_image_height * 0.5)
 
 	if not erase:
 		mask_image.blend_rect(light_brush, light_brush.get_used_rect(), pos - offset)
@@ -95,17 +128,29 @@ func _process(_delta):
 	if Input.is_action_pressed("quit"):
 		get_tree().quit()
 
-	# queue_redraw()
+	queue_redraw()
 
 func _input(event: InputEvent) -> void:
+	if current_file_path == "":
+		return
 
 	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_C:
-			black_circle_bool = not black_circle_bool
-			settings_menu.set_item_checked(0, black_circle_bool)
+		if event.pressed:
+			if event.keycode == KEY_C:
+				black_circle_bool = not black_circle_bool
+				settings_menu.set_item_checked(0, black_circle_bool)
 
-		if event.pressed and event.keycode == KEY_T:
-			update_fog_texture(Color.BLACK)
+			if event.keycode == KEY_T:
+				fog_color_index = (fog_color_index + 1) % len(FOG_COLOR_LIST)
+				update_fog_texture(FOG_COLOR_LIST[fog_color_index])
+
+			if event.keycode == KEY_P:
+				performance_mode = not performance_mode
+				if performance_mode:
+					Engine.max_fps = 30
+				else:
+					Engine.max_fps = 60
+				settings_menu.set_item_checked(1, performance_mode)
 
 	if event.is_action_pressed('mod'):
 		mod_held = true
@@ -119,7 +164,16 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed('zoom_out') and mod_held:
 		update_brushes(5)
 
+	if event.is_action_pressed('save'):
+		if current_file_path != "":
+			if current_file_path.ends_with('.map'):
+				write_map(current_file_path)
+			else:
+				save_dialog.popup()
+
+
 	if event is InputEventMouseButton:
+		var pos = get_global_mouse_position()
 		if hovering_over_gui:
 			return
 
@@ -127,19 +181,22 @@ func _input(event: InputEvent) -> void:
 			m1_pressed = event.pressed
 
 			if m1_pressed:
-				update_mask()
+				update_mask(pos)
 
 		if event.button_index == MOUSE_BUTTON_RIGHT:
 			m2_pressed = event.pressed
 
 			if m2_pressed:
-				update_mask(true)
+				update_mask(pos, true)
+		prev_mouse_pos = pos
 
 	elif event is InputEventMouseMotion:
+		var pos = get_global_mouse_position()
 		if m1_pressed:
-			update_mask()
+			update_mask_wrapper(pos)
 		elif m2_pressed:
-			update_mask(true)
+			update_mask_wrapper(pos, true)
+		prev_mouse_pos = pos
 
 func _draw() -> void:
 	var circle_color : Color
@@ -151,16 +208,38 @@ func _draw() -> void:
 	draw_arc(get_global_mouse_position(), brush_size * 0.5, 0.0, 2 * 3.141592, 100, circle_color, 1)
 	draw_circle(get_global_mouse_position(), brush_size * 0.5, circle_color, false, 1)
 
-func update_fog_texture(color : Color):
-	var fog_image = Image.create(map_image_width * fog_scaling, map_image_height * fog_scaling, false, Image.FORMAT_RGBAH)
-	mask_image.fill(color)
-	var fog_image_texture = ImageTexture.create_from_image(fog_image)
+func update_fog_texture(color):
+	var fog_image_texture
+	if color == null:
+		fog_image_texture = FogTexture
+		RenderingServer.set_default_clear_color(Color.BLACK)
+
+	else:
+		var fog_image = Image.create(map_image_width, map_image_height, false, Image.FORMAT_RGBAH)
+		fog_image.fill(color)
+		fog_image_texture = ImageTexture.create_from_image(fog_image)
+		RenderingServer.set_default_clear_color(color)
+
 	player_fog.texture = fog_image_texture
 	dm_fog.texture = fog_image_texture
 
 
 
 func load_map(path: String) -> void:
+	if not (
+		path.ends_with('.jpg') or
+		path.ends_with('.jpeg') or
+		path.ends_with('.png') or
+		path.ends_with('.map')
+	):
+		warning.title = "Invalid file format"
+		warning.dialog_text = "File must be .jpg, .jpeg, .png or .map"
+		warning.popup_centered()
+		return
+
+
+	current_file_path = path
+
 	if path.ends_with(".map"):
 		var reader = ZIPReader.new()
 		reader.open(path)
@@ -186,7 +265,7 @@ func load_map(path: String) -> void:
 		map_image_height = map_image.get_size()[1]
 
 
-		mask_image = Image.create(map_image_width * fog_scaling, map_image_height * fog_scaling, false, Image.FORMAT_RGBAH)
+		mask_image = Image.create(map_image_width, map_image_height, false, Image.FORMAT_RGBAH)
 		mask_image.fill(Color(0, 0, 0, 1))
 
 	mask_texture = ImageTexture.create_from_image(mask_image)
@@ -205,25 +284,7 @@ func load_map(path: String) -> void:
 	dm_background.texture = image_texture
 	player_background.texture = image_texture
 
-func _on_file_id_pressed(id: int) -> void:
-	if id == 0:
-		file_dialog.popup()
-
-	if id == 1:
-		save_dialog.popup()
-
-	if id == 2:
-		get_tree().quit()
-
-func _on_settings_id_pressed(id: int) -> void:
-	if id == 0:
-		black_circle_bool = not black_circle_bool
-		settings_menu.set_item_checked(id, black_circle_bool)
-
-func _on_file_dialog_file_selected(path: String) -> void:
-	load_map(path)
-
-func _on_file_dialog_2_file_selected(path:String) -> void:
+func write_map(path: String) -> void:
 	var writer = ZIPPacker.new()
 	writer.open(path)
 	writer.start_file("mask.png")
@@ -233,6 +294,46 @@ func _on_file_dialog_2_file_selected(path:String) -> void:
 	writer.close_file()
 
 	writer.close()
+
+func _on_file_id_pressed(id: int) -> void:
+	if id == 0:
+		load_dialog.popup()
+
+	if id == 1:
+		if current_file_path == "":
+			warning.title = "Cannot save an empty map"
+			warning.dialog_text = "Cannot save an empty map"
+			warning.popup_centered()
+		else:
+			save_dialog.popup()
+
+	if id == 2:
+		warning.title = "Keybindings"
+		warning.dialog_text = "General\n    Left click: Reveal areas\n    Right click: Hide areas\n    Middle mouse: Pan/Move view\n    Mouse wheel: Zoom\n    Shift+Mouse wheel: Resize brush\n    Ctrl+S: Save\nKeybinds\n    T: Toggle between fog colors\n    C: Change color of circle\n    P: Toggle performance mode"
+		warning.popup_centered()
+
+	if id == 3:
+		get_tree().quit()
+
+func _on_settings_id_pressed(id: int) -> void:
+	if id == 0:
+		black_circle_bool = not black_circle_bool
+		settings_menu.set_item_checked(id, black_circle_bool)
+
+	if id == 1:
+		performance_mode = not performance_mode
+		if performance_mode:
+			Engine.max_fps = 30
+		else:
+			Engine.max_fps = 60
+		settings_menu.set_item_checked(id, performance_mode)
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	load_map(path)
+
+func _on_file_dialog_2_file_selected(path:String) -> void:
+	write_map(path)
+
 
 func _on_menu_bar_mouse_entered() -> void:
 	hovering_over_gui = true
